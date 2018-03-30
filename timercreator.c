a20d2e04d34d3f97843dcfac33f90b019267cbb5
xrpan <xiaopan@indiana.edu>
2018-03-30 18:28:13 -0400

    interval 1

--- timercreator.c
+++ timercreator.c
@@ -90,7 +90,7 @@ void testTimerSign(){
     begin = rdtsc();
     
     ts.it_interval.tv_sec = 0;
-    ts.it_interval.tv_nsec = 50;  
+    ts.it_interval.tv_nsec = 1;  
     ts.it_value.tv_sec = 0;
     ts.it_value.tv_nsec = 5;  
     //printTime();
