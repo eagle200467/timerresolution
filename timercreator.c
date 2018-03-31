18c815e4025f542226feee01791eb97965b91a6a
xrpan <xiaopan@indiana.edu>
2018-03-31 17:53:02 -0400

    interval half second

--- timercreator.c
+++ timercreator.c
@@ -92,7 +92,7 @@ void testTimerSign(){
     int begin, end;
     begin = rdtsc();
     
-    ts.it_interval.tv_sec = 1;
+    ts.it_interval.tv_sec = 0.5;
     ts.it_interval.tv_nsec = 0;  
     ts.it_value.tv_sec = 0;
     ts.it_value.tv_nsec = 0;  
